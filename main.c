d2c1960e846b63abab9a8af34b6b020a135ca8a4
Steve Choo <pliable@gmail.com>
2015-07-21 01:54:54 -0700

    babby steps yo

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+   * tic-tac-toe
+   * -----------
+   * breaking the rust bruh
+   *
+   * @author Steve Choo
+*/
+int main(int argc, char *argv[]) {
+   
+   return 0;
+}
