a64aa121b0c9f0fc92362cb7b73aec8745207365
Steve Choo <pliable@gmail.com>
2015-07-21 02:45:06 -0700

    moved prompt() into its own file, added draw.c/h

---
+++ draw.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void draw_board(char new_position, int which_player, int* board) {
+
+
+}
+void prompt() {
+   printf("Hello, welcome to this game of the tic tac toens\n");
+   printf("------------------------------------------------\n");
+   printf("                1 - Start game\n");
+   printf("                2 - Quit\n");
+   printf("                3 - Credits\n");
+   printf("Your number is my command, master: ");
+}
---
+++ draw.h
@@ -0,0 +1,2 @@
+void prompt();
+void draw_board(char new_position, int which_player, int* board);
